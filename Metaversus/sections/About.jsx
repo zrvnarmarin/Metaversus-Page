8b8125e997f07f6ede168198c0c67e26aa3dc7d2
zrvnarmarin <76marin76@gmail.com>
2022-12-03 00:19:28 +0100

    hero section finished, about section started

--- Metaversus/sections/About.jsx
+++ Metaversus/sections/About.jsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
 const About = () => (
   <section>
     About section
